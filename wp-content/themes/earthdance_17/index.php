5dba26bf0139998755ffdf2adb7021b666ef25a7
Benjamin Gandhi-Shepard <812studio@solvm-mbp.home>
2017-03-02 15:39:47 -0600

    working on html

--- wp-content/themes/earthdance_17/index.php
+++ wp-content/themes/earthdance_17/index.php
@@ -1,14 +1,96 @@
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<title>TEarthdance</title>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" href="/wp-content/themes/earthdance_17/style.css" />
-</head>
-<body>
-
-<h1>Hello from WP</h1>
 
 
-</body>
-</html>
+
+
+	<!doctype html>
+	<html class="no-js" lang="">
+	    <head>
+	        <meta charset="utf-8">
+	        <meta http-equiv="x-ua-compatible" content="ie=edge">
+	        <title></title>
+	        <meta name="description" content="">
+	        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+					<link rel="stylesheet" href="/wp-content/themes/earthdance_17/style.css" />
+	        <!-- <link rel="apple-touch-icon" href="apple-touch-icon.png"> -->
+	        <!-- Place favicon.ico in the root directory -->
+	        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+	    </head>
+	    <body>
+
+				<!--header section-->
+				<div class="header__layer">
+				  <div class="header__container">
+				    <div class="header__item"><a class="header__logo">EarthDance
+			        <svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 148.7 149.4">
+			          <style>.st0{fill:#231F20;}</style>
+			          <path d="M66 116.6c5-2.9 8.4-4.8 10.2-5.8-4.8-6.4-6.3-9.2-6.7-10.1-.1-.2-.2-.4-.3-.7-2.7 3.9-6.7 6.9-11.9 8.8-4.3 1.6-9.2 2.4-14.4 2.4-6.7 0-13.7-1.3-20.3-3.8 7.9 13.2 20.7 23.1 35.8 27.3l-.3-3.2c-.3-2-1.2-9.6 7.9-14.9z" class="st0"></path>
+			          <path d="M54.6 101.8c4.6-1.7 7.8-4.5 9.5-8.1 2.3-5.2 4.5-10.3 6.6-15.3l2.4-5.7c-.1-.3-.1-.5-.2-.7-24.3-12.5-23.4-41-22.3-50.7-16.3 7-28.8 21-33.9 38.2.7-.4 1.4-.8 2.2-1.2 21.5-11.5 30.7-.5 33 3.2 3.5 5.6 4.1 13.5 1.5 19.7-2.1 5-6 8.3-11 9.4-6.1 1.3-11.6 0-15.2-3.6-2.6-2.7-3.8-6.4-3.1-9.5v-.1c1.2-4.3 3.7-6 5.5-6.6 4.5-1.6 9 1.5 9.5 1.9 1.7 1.2 2.1 3.6.9 5.3-1.2 1.7-3.6 2.1-5.3.9-.8-.6-2.2-1.1-2.6-.9-.1 0-.4.4-.7 1.4-.1.4.1 1.4 1.1 2.4s3.4 2.6 8.2 1.5c2.6-.6 4.5-2.2 5.6-5 1.6-3.9 1.3-9.3-.9-12.7-3.9-6.1-12.2-6.3-23-.6-3.5 1.9-6.1 3.9-7.9 5.8-.2 1.9-.3 3.9-.3 5.9 0 6.8 1.2 13.4 3.3 19.5 1.5 1.1 3.2 2.1 5.3 3.1 10.4 4.8 23 5.8 31.8 2.5zM78.5 56.6c4.3-13.5 5.1-22.1 4.4-24.9-2.5 1.3-4.8 12.6-4.4 24.9z" class="st0"></path>
+			          <path d="M71.6 39c.1.2.2.5.3.8 1.2-7 3.5-12.7 7.4-14.8 4.6-2.4 7.5-.5 8.5.4 2.9 2.6 3.3 8.2 2.4 14.9 6-7.9 11.8-12.1 16.7-14.3-9.4-6-20.5-9.5-32.5-9.5-6.7 0-13.2 1.1-19.2 3.2 5.6 4.2 15 14.9 16.4 19.3zM80.9 116.9c-1.2.6-4.9 2.6-11.1 6.2-5.1 3-4.2 6.7-4.2 6.7l.1.3.6 6.1c2.7.4 5.4.6 8.2.6 1.3 0 2.5-.1 3.8-.1l2-2.7c.3-.3 7.3-8.8 1-16.7l-.4-.4z" class="st0"></path>
+			          <path d="M114.4 31.5c-4.1.7-10.9 3.5-18.6 14-7 9.5-12.4 22.3-18.1 35.8-.2.5-.4.9-.6 1.4-3.3 9.6-1 14.4-.9 14.4l.2.4c0 .1 1.8 3.7 10.8 15 6.5 8.1 4.5 17 1.4 22.6 15.5-3.7 28.7-13.5 36.9-26.6-5 1.8-10.5 1.1-15.8-2.2C96.9 98.4 94 83.5 97 72.3c3.2-12.3 10-19.9 17.8-19.9h.1c5.9 0 11.2 5 12.2 11.5 1 6.6-2.6 12.3-9.3 14.4-2 .6-4.1-.5-4.8-2.5s.5-4.1 2.5-4.8c4.2-1.3 4.4-4.4 4.1-6-.4-2.5-2.2-5-4.7-5-4 0-8.3 5.8-10.5 14.2-2.1 8.3-.1 19.8 9.4 25.7 3.4 2.1 6.6 2.6 9.4 1.5 4.3-1.7 8.2-7 10.8-14.7.5-3.3.9-6.6.9-10.1-.2-17.9-8.1-34-20.5-45.1z" class="st0"></path>
+			          <path d="M142.2 45.8C127.4 11.3 84-11.3 46.3 5.9 10.2 16.1-3.1 62.2.6 94.5 6 140.9 58.3 158 102.4 145.4c45-13 54.8-64.2 39.8-99.6zm-67.8 98.5C37 144.3 6.7 114 6.7 76.6 6.7 39.2 37 8.9 74.4 8.9s67.7 30.3 67.7 67.7c0 37.4-30.3 67.7-67.7 67.7z" class="st0"></path>
+			        </svg></a>
+				      <p class="header__tagline">organic farm school</p>
+				    </div>
+				    <div class="header__item"><a class="header__link">233 Dade Ave. Ferguson</a><a class="header__link">314-521-1006</a><a class="header__link">fb</a><a class="header__link">tw</a><a class="header__link">yt</a></div>
+				  </div>
+				</div>
+
+				<!--feature section-->
+				<div class="feature__layer">
+				  <div class="feature__container">
+				    <div class="feature__nav"><a class="feature__link">About</a><a class="feature__link">Food</a><a class="feature__link">Farmers</a><a class="feature__link">Community</a><a class="feature__link">Support</a><a class="feature__link">News</a><a class="feature__link">Jobs</a><a class="feature__link">Contact</a></div>
+				    <div class="feature__main">
+				      <svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 148.7 149.4">
+				        <style>.st0{fill:#231F20;}</style>
+				        <path d="M66 116.6c5-2.9 8.4-4.8 10.2-5.8-4.8-6.4-6.3-9.2-6.7-10.1-.1-.2-.2-.4-.3-.7-2.7 3.9-6.7 6.9-11.9 8.8-4.3 1.6-9.2 2.4-14.4 2.4-6.7 0-13.7-1.3-20.3-3.8 7.9 13.2 20.7 23.1 35.8 27.3l-.3-3.2c-.3-2-1.2-9.6 7.9-14.9z" class="st0"></path>
+				        <path d="M54.6 101.8c4.6-1.7 7.8-4.5 9.5-8.1 2.3-5.2 4.5-10.3 6.6-15.3l2.4-5.7c-.1-.3-.1-.5-.2-.7-24.3-12.5-23.4-41-22.3-50.7-16.3 7-28.8 21-33.9 38.2.7-.4 1.4-.8 2.2-1.2 21.5-11.5 30.7-.5 33 3.2 3.5 5.6 4.1 13.5 1.5 19.7-2.1 5-6 8.3-11 9.4-6.1 1.3-11.6 0-15.2-3.6-2.6-2.7-3.8-6.4-3.1-9.5v-.1c1.2-4.3 3.7-6 5.5-6.6 4.5-1.6 9 1.5 9.5 1.9 1.7 1.2 2.1 3.6.9 5.3-1.2 1.7-3.6 2.1-5.3.9-.8-.6-2.2-1.1-2.6-.9-.1 0-.4.4-.7 1.4-.1.4.1 1.4 1.1 2.4s3.4 2.6 8.2 1.5c2.6-.6 4.5-2.2 5.6-5 1.6-3.9 1.3-9.3-.9-12.7-3.9-6.1-12.2-6.3-23-.6-3.5 1.9-6.1 3.9-7.9 5.8-.2 1.9-.3 3.9-.3 5.9 0 6.8 1.2 13.4 3.3 19.5 1.5 1.1 3.2 2.1 5.3 3.1 10.4 4.8 23 5.8 31.8 2.5zM78.5 56.6c4.3-13.5 5.1-22.1 4.4-24.9-2.5 1.3-4.8 12.6-4.4 24.9z" class="st0"></path>
+				        <path d="M71.6 39c.1.2.2.5.3.8 1.2-7 3.5-12.7 7.4-14.8 4.6-2.4 7.5-.5 8.5.4 2.9 2.6 3.3 8.2 2.4 14.9 6-7.9 11.8-12.1 16.7-14.3-9.4-6-20.5-9.5-32.5-9.5-6.7 0-13.2 1.1-19.2 3.2 5.6 4.2 15 14.9 16.4 19.3zM80.9 116.9c-1.2.6-4.9 2.6-11.1 6.2-5.1 3-4.2 6.7-4.2 6.7l.1.3.6 6.1c2.7.4 5.4.6 8.2.6 1.3 0 2.5-.1 3.8-.1l2-2.7c.3-.3 7.3-8.8 1-16.7l-.4-.4z" class="st0"></path>
+				        <path d="M114.4 31.5c-4.1.7-10.9 3.5-18.6 14-7 9.5-12.4 22.3-18.1 35.8-.2.5-.4.9-.6 1.4-3.3 9.6-1 14.4-.9 14.4l.2.4c0 .1 1.8 3.7 10.8 15 6.5 8.1 4.5 17 1.4 22.6 15.5-3.7 28.7-13.5 36.9-26.6-5 1.8-10.5 1.1-15.8-2.2C96.9 98.4 94 83.5 97 72.3c3.2-12.3 10-19.9 17.8-19.9h.1c5.9 0 11.2 5 12.2 11.5 1 6.6-2.6 12.3-9.3 14.4-2 .6-4.1-.5-4.8-2.5s.5-4.1 2.5-4.8c4.2-1.3 4.4-4.4 4.1-6-.4-2.5-2.2-5-4.7-5-4 0-8.3 5.8-10.5 14.2-2.1 8.3-.1 19.8 9.4 25.7 3.4 2.1 6.6 2.6 9.4 1.5 4.3-1.7 8.2-7 10.8-14.7.5-3.3.9-6.6.9-10.1-.2-17.9-8.1-34-20.5-45.1z" class="st0"></path>
+				        <path d="M142.2 45.8C127.4 11.3 84-11.3 46.3 5.9 10.2 16.1-3.1 62.2.6 94.5 6 140.9 58.3 158 102.4 145.4c45-13 54.8-64.2 39.8-99.6zm-67.8 98.5C37 144.3 6.7 114 6.7 76.6 6.7 39.2 37 8.9 74.4 8.9s67.7 30.3 67.7 67.7c0 37.4-30.3 67.7-67.7 67.7z" class="st0"></path>
+				      </svg>
+				      <h1 class="feature__title">EarthDance</h1>
+				      <h2 class="feature__tagline">organic farm school</h2>
+				    </div>
+				  </div>
+				</div>
+
+				<!--zipper section-->
+				<div class="zipper__layer">
+				  <div class="zipper__container">
+				    <h2 class="zipper__heading">We grow food, farmers and community</h2>
+				    <div class="zpper__columns">
+				      <h3 class="zipper__heading">Food</h3>
+				      <div class="zipper__content">
+				        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+				        <tempor>incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</tempor>
+				        <quis>nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</quis>
+				        <consequat></consequat>
+				        <cillum>dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non</cillum>
+				        <proident>, sunt in culpa qui officia deserunt mollit anim id est laborum.</proident><a href="x" class="zipper__button">read more</a>
+				      </div>
+				    </div>
+				    <div class="zpper__columns">
+				      <h3 class="zipper__heading">Food</h3>
+				      <div class="zipper__content">
+				        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+				        <tempor>incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</tempor>
+				        <quis>nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</quis>
+				        <consequat></consequat>
+				        <cillum>dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non</cillum>
+				        <proident>, sunt in culpa qui officia deserunt mollit anim id est laborum.</proident><a href="x" class="zipper__button">read more</a>
+				      </div>
+				    </div>
+				    <div class="zpper__columns">
+				      <h3 class="zipper__heading">Food</h3>
+				      <div class="zipper__content">
+				        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+				        <tempor>incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</tempor>
+				        <quis>nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</quis>
+				        <consequat></consequat>
+				        <cillum>dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non</cillum>
+				        <proident>, sunt in culpa qui officia deserunt mollit anim id est laborum.</proident><a href="x" class="zipper__button">read more</a>
+				      </div>
+				    </div>
+				  </div>
+				</div>
