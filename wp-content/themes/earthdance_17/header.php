d7341ff1cea384b55206f99b595bdc819793cf41
Mike Shepard <sheps1632@gmail.com>
2017-03-07 22:40:52 -0600

    styled header but need to touch up

--- wp-content/themes/earthdance_17/header.php
+++ wp-content/themes/earthdance_17/header.php
@@ -7,6 +7,12 @@
 	        <meta name="description" content="">
 	        <meta name="viewport" content="width=device-width, initial-scale=1">
 
+					<script src="https://use.typekit.net/jec1rlg.js"></script>
+					<script>try{Typekit.load({ async: true });}catch(e){}</script>
+
+					<link rel="stylesheet" href="https://cdn.jsdelivr.net/evil-icons/1.9.0/evil-icons.min.css">
+					<script src="https://cdn.jsdelivr.net/evil-icons/1.9.0/evil-icons.min.js"></script>
+
 					<link rel="stylesheet" href="/wp-content/themes/earthdance_17/style.css" />
 	        <!-- <link rel="apple-touch-icon" href="apple-touch-icon.png"> -->
 	        <!-- Place favicon.ico in the root directory -->
